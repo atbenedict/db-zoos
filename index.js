ec4e4ce54ec5772bcd44b9c2ca5e3ab3d9bd3596
Andrew Benedict <atbenedict@gmail.com>
2019-02-19 18:42:07 -0500

    added bears :)

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ server.get("/api/zoos", async (req, res) => {
   }
 });
 
+const errors = {
+  "19": "Another record with that value exists"
+};
+
 // list a zoo by id
 server.get("/api/zoos/:id", async (req, res) => {
   try {
@@ -37,10 +41,6 @@ server.get("/api/zoos/:id", async (req, res) => {
     res.status(500).json(error);
   }
 });
-
-const errors = {
-  "19": "Another record with that value exists"
-};
 
 // create a zoo
 server.post("/api/zoos", async (req, res) => {
@@ -81,6 +81,78 @@ server.put("/api/zoos/:id", async (req, res) => {
 server.delete("/api/zoos/:id", async (req, res) => {
   try {
     const count = await db("zoos")
+      .where({ id: req.params.id })
+      .del();
+
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: "Records not found" });
+    }
+  } catch (error) {}
+});
+
+// list all bears
+server.get("/api/bears", async (req, res) => {
+  try {
+    const bears = await db("bears");
+    res.status(200).json(bears);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// list a bear by id
+server.get("/api/bears/:id", async (req, res) => {
+  try {
+    const bear = await db("bears")
+      .where({ id: req.params.id })
+      .first();
+    res.status(200).json(bear);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// create a bear
+server.post("/api/bears", async (req, res) => {
+  try {
+    const [id] = await db("bears").insert(req.body);
+
+    const bear = await db("bears")
+      .where({ id })
+      .first();
+
+    res.status(201).json(bear);
+  } catch (error) {
+    const message = errors[error.errno] || "We ran into an error";
+    res.status(500).json({ message, error });
+  }
+});
+
+// update a bear
+server.put("/api/bears/:id", async (req, res) => {
+  try {
+    const count = await db("bears")
+      .where({ id: req.params.id })
+      .update(req.body);
+
+    if (count > 0) {
+      const bear = await db("bears")
+        .where({ id: req.params.id })
+        .first();
+
+      res.status(200).json(bear);
+    } else {
+      res.status(404).json({ message: "Records not found" });
+    }
+  } catch (error) {}
+});
+
+// remove bears (inactivate the bear)
+server.delete("/api/bears/:id", async (req, res) => {
+  try {
+    const count = await db("bears")
       .where({ id: req.params.id })
       .del();
 
