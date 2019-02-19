9e102e07fe7c7286b0bd0d0aec66f5fe3c483f42
Andrew Benedict <atbenedict@gmail.com>
2019-02-19 16:18:10 -0500

    MVP done

--- index.js
+++ index.js
@@ -1,12 +1,96 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+
+const knexConfig = {
+  client: "sqlite3",
+  connection: {
+    filename: "./data/lambda.sqlite3"
+  },
+  useNullAsDefault: true
+};
+const db = knex(knexConfig);
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+// list all zoos
+server.get("/api/zoos", async (req, res) => {
+  try {
+    const zoos = await db("zoos");
+    res.status(200).json(zoos);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// list a zoo by id
+server.get("/api/zoos/:id", async (req, res) => {
+  try {
+    const zoo = await db("zoos")
+      .where({ id: req.params.id })
+      .first();
+    res.status(200).json(zoo);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+const errors = {
+  "19": "Another record with that value exists"
+};
+
+// create a zoo
+server.post("/api/zoos", async (req, res) => {
+  try {
+    const [id] = await db("zoos").insert(req.body);
+
+    const zoo = await db("zoos")
+      .where({ id })
+      .first();
+
+    res.status(201).json(zoo);
+  } catch (error) {
+    const message = errors[error.errno] || "We ran into an error";
+    res.status(500).json({ message, error });
+  }
+});
+
+// update a zoo
+server.put("/api/zoos/:id", async (req, res) => {
+  try {
+    const count = await db("zoos")
+      .where({ id: req.params.id })
+      .update(req.body);
+
+    if (count > 0) {
+      const zoo = await db("zoos")
+        .where({ id: req.params.id })
+        .first();
+
+      res.status(200).json(zoo);
+    } else {
+      res.status(404).json({ message: "Records not found" });
+    }
+  } catch (error) {}
+});
+
+// remove zoos (inactivate the zoo)
+server.delete("/api/zoos/:id", async (req, res) => {
+  try {
+    const count = await db("zoos")
+      .where({ id: req.params.id })
+      .del();
+
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: "Records not found" });
+    }
+  } catch (error) {}
+});
 
 const port = 3300;
 server.listen(port, function() {
